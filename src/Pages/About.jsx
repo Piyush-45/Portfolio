795afe6269d0b6b3d5dc1971bf73fe0daf5f7353
Piyush Tyagi <98485162+Piyush-45@users.noreply.github.com>
2024-01-02 21:18:22 +0530

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -10,8 +10,7 @@ import api from "../images/apis.png";
 import mongodb from "../images/mongodb.jpeg";
 // import expressjs from "../images/epxressjs.jpeg";
 import nodejs from "../images/nodejs.jpeg";
-import express from "../images/expressjs.jpeg
-";
+import express from "../images/expressjs.jpeg";
 import github from "../images/github color.png";
 import webdesign from "../images/web design.png";
 import firebase from "../images/firebase2.png";
