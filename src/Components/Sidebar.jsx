331225f182b42cbe820338e3425bfd7777cc5aed
Piyush Tyagi <98485162+Piyush-45@users.noreply.github.com>
2024-01-05 10:15:14 +0530

    Update Sidebar.jsx

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -51,7 +51,7 @@ const Sidebar = () => {
           </li>
         </ul>
       </div>
-      <a href="https://drive.google.com/file/d/17zUvt6rsKVO0ERnZPqX6b8LY8LYV8bzo/view?usp=sharing" target="_blank" className="cv_btn">
+      <a href="https://drive.google.com/file/d/1dX0aQF_tZr3N0UYCYfq2J_NAilzlv4wy/view?usp=sharing" className="cv_btn">
         Download CV
       </a>
       <div className="copyright">© 2022 All rights reserved.</div>
